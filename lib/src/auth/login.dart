de6ed73ab6a3f1458e3d1f1b4bbd05e9b23fc358
Sithu Aung <sithuaungdev01@gmail.com>
2022-10-14 17:50:00 +0630

    Login Integration

--- lib/src/auth/login.dart
+++ lib/src/auth/login.dart
@@ -1,4 +1,11 @@
+import 'dart:convert';
+import 'dart:developer';
+import 'package:bouncing_widget/bouncing_widget.dart';
+import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
+import 'package:learning/data/response/auth_response.dart';
+import 'package:learning/src/main_map/main_map1.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class LoginScreen extends StatefulWidget {
   static const routeName = '/login';
@@ -19,8 +26,201 @@ class _LoginScreenState extends State<LoginScreen> {
     return MediaQuery(
       data: mqDataNew,
       child: Scaffold(
-        body: Container(),
+        body: Stack(
+          children: [
+            Image.asset(
+              "assets/images/app_back.jpg",
+              width: size.width,
+              height: size.height,
+              fit: BoxFit.fill,
+            ),
+            Row(
+              children: [
+                Stack(
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.only(
+                        left: size.width * 0.1,
+                        top: size.width * 0.1,
+                      ),
+                      child: Stack(
+                        children: [
+                          Image.asset(
+                            "assets/images/wooden_four.png",
+                            width: size.width * 0.45,
+                            height: size.height * 0.75,
+                            fit: BoxFit.fill,
+                          ),
+                          Padding(
+                            padding: const EdgeInsets.only(top: 24),
+                            child: Image.asset(
+                              "assets/images/paper_medium_leafs.png",
+                              width: size.width * 0.42,
+                              height: size.height * 0.67,
+                              fit: BoxFit.fill,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(
+                        left: size.width * 0.65,
+                        top: 26,
+                      ),
+                      child: Image.asset(
+                        "assets/images/app_logo.png",
+                        width: size.width * 0.25,
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(
+                        left: size.width * 0.53,
+                        top: size.height * 0.47,
+                      ),
+                      child: Image.asset(
+                        "assets/images/wooden_small.png",
+                        width: size.width * 0.35,
+                        height: 100,
+                        fit: BoxFit.fill,
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(
+                        left: size.width * 0.56,
+                        top: size.height * 0.49,
+                      ),
+                      child: Image.asset(
+                        "assets/images/papersmall.png",
+                        width: size.width * 0.31,
+                        height: 86,
+                        fit: BoxFit.fill,
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(
+                        left: size.width * 0.58,
+                        top: size.height * 0.51,
+                      ),
+                      child: Stack(
+                        children: [
+                          Image.asset(
+                            "assets/images/button_green_round.png",
+                            width: size.width * 0.26,
+                            height: 68,
+                            fit: BoxFit.fill,
+                          ),
+                          const Padding(
+                            padding: EdgeInsets.only(left: 32, top: 16),
+                            child: Text(
+                              "Create New Account\nအကောင့်သစ်ဖွင့်မယ်",
+                              textAlign: TextAlign.center,
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 15,
+                                height: 1.2,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(
+                        left: size.width * 0.48,
+                        top: size.height * 0.46,
+                      ),
+                      child: Image.asset(
+                        "assets/images/wooden_ring_2.png",
+                        width: 68,
+                      ),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(
+                        left: size.width * 0.11,
+                        top: size.width * 0.06,
+                      ),
+                      child: BouncingWidget(
+                        duration: const Duration(milliseconds: 400),
+                        scaleFactor: 1.5,
+                        onPressed: () async {
+                          login(context, "sithuaung", "123456");
+                        },
+                        child: Stack(
+                          children: [
+                            Image.asset(
+                              "assets/images/title_leaf.png",
+                              width: size.width * 0.38,
+                            ),
+                            const Padding(
+                              padding: EdgeInsets.only(left: 70, top: 24),
+                              child: Text(
+                                "Login/လော့ဂ်အင် ဝင်မယ်",
+                                textAlign: TextAlign.center,
+                                style: TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 17,
+                                    height: 1.2,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ],
+            )
+          ],
+        ),
       ),
     );
+  }
+
+  login(BuildContext context, String name, String password) async {
+    Map<String, String> headers = {
+      "Accept": "application/json",
+      'content-type': 'application/json',
+    };
+
+    log('https://wwf.360ed.com/api/login');
+    final response = await http.post(
+        Uri.parse('https://wwf.360ed.com/api/login'),
+        headers: headers,
+        body: jsonEncode(<String, String>{
+          'username': name,
+          'password': password,
+          'device_token': 'test'
+        }));
+    log(jsonEncode(<String, String>{
+      'phone': name,
+      'password': password,
+      'device_token': 'test'
+    }));
+    print(response.statusCode);
+    log(response.body);
+    if (response.statusCode == 200) {
+      AuthResponse data = AuthResponse.fromJson(jsonDecode(response.body));
+      SharedPreferences prefs = await SharedPreferences.getInstance();
+      prefs.setString("token", data.apiToken.toString());
+      prefs.setString("name", data.user!.userName!.toString());
+      prefs.setString("age", data.user!.age!.toString());
+      prefs.setString("phone", data.user!.phone!.toString());
+      prefs.setString("email", data.user!.email!.toString());
+      prefs.setString("profession", data.user!.profession!.toString());
+
+      prefs.setString("user_type", data.user!.userType!.toString());
+      prefs.setString("last_login", data.user!.lastLogin!.toString());
+      prefs.setString(
+          "is_email_verified", data.user!.isEmailVerified!.toString());
+      prefs.setString("updated_at", data.user!.updatedAt!.toString());
+      prefs.setString("created_at", data.user!.createdAt!.toString());
+      prefs.setString("s_id", data.user!.sId!.toString());
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => const MainMapFirstScreen()),
+      );
+    }
   }
 }
